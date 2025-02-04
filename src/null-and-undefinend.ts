58e42dabe5b188deb6d57df8771171e8e7e94f9c
toketime <flatoke@gmail.com>
2025-02-04 14:41:13 +0900

    intersection type

--- src/null-and-undefinend.ts
+++ src/null-and-undefinend.ts
@@ -3,3 +3,7 @@ export {};
 let absence: null = null;
 // absence = 'hello';
 let data: undefined = undefined;
+// data = 123;
+//これはエラーになる
+//undefinedはnullと違って、どんな型でも許可する
+//そのため、nullと違って、型を定義しないといけない
